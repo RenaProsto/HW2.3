1c0d192b4f062e5940248c1eb6e73973a788c9f1
RenaProsto <renaprosto123@gmail.com>
2023-06-17 21:04:17 +0500

    Готово ДЗ

---
+++ src/ServiceStation.java
@@ -0,0 +1,60 @@
+public abstract class ServiceStation {
+    private String modelName;
+
+    private int wheelsCount;
+
+    public  ServiceStation(String modelName,int wheelsCount){
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    @Override
+    public String toString() {
+        return "modelName='" + modelName + '\'' +
+                ", wheelsCount=" + wheelsCount ;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+    public void check(Car car, Bicycle bicycle, Truck truck) {
+        if (car != null) {
+            System.out.println("Обслуживаем " + car.getModelName());
+            for (int i = 0; i < car.getWheelsCount(); i++) {
+                car.updateTyre();
+            }
+            car.checkEngine();
+        } else if (truck != null) {
+            System.out.println("Обслуживаем " + truck.getModelName());
+            for (int i = 0; i < truck.getWheelsCount(); i++) {
+                truck.updateTyre();
+            }
+            truck.checkEngine();
+            truck.checkTrailer();
+        } else if (bicycle != null) {
+            System.out.println("Обслуживаем " + bicycle.getModelName());
+            for (int i = 0; i < bicycle.getWheelsCount(); i++) {
+                bicycle.updateTyre();
+            }
+        }
+    }
+
+
+
+}
